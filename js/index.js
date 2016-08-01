a76d7c90800cd071cc4e805313b3b2d88c9c8dbe
Anthony <antcho92@gmail.com>
2016-07-31 18:35:16 -0700

    starting to add timer functionality

--- js/index.js
+++ js/index.js
@@ -1,44 +1,66 @@
-$(document).ready(function() {
-    //sets the starting timer at 25
-    $("#timer").text($('#sessionLength').text());
-    //experimenting with multiple selectors, having trouble evaluating the string "+"
-    /*$("#increase").click(function() {
-        var modifier = $(this).text();
-        if (this == "#increaseSession" || this == "#decreaseSession") {
-            var result = $('#sessionLength').text() + modifier + 1;
-            $('#sessionLength').text(eval(result));
-        } else if (this == '#decreaseBreak' || this == "#increaseBreak") {
-            var result = $('#breakLength').text() + modifier + 1;
-            $('#breakLength').text(eval(result));
+function startTimer() {
+    var start = new Date().getTime(),
+    time = $('#timer').text(),
+    elapsed = '0.0';
+    function instance() {
+        time += 100;
+        elapsed = Math.floor(time/100)/10;
+        if(Math.round(elapsed) == elapsed) {
+            elapsed += '.0';
         }
-    }); */
 
-    //increases the session length by 1 minutes
-    $("#increaseSession").click(function() {
-        $('#mode').text("Session");
-        var next = parseInt($("#sessionLength").text(), 10);
-        $("#sessionLength").text(next + 1);
-        $("#timer").text($('#sessionLength').text());
-    });
-    //decreases the session length by 1 minute
-    $("#decreaseSession").click(function() {
-        $('#mode').text("Session");
-        var next = parseInt($("#sessionLength").text(), 10);
-        $("#sessionLength").text(next - 1);
-        $("#timer").text($('#sessionLength').text());
-    });
-    //increases the break length by 1 minute
-    $("#increaseBreak").click(function() {
-        $('#mode').text("Break");
-        var next = $("#breakLength").text();
-        $("#breakLength").text(parseInt(next, 10) + 1);
-        $("#timer").text($('#breakLength').text());
-    });
-    //decreases break length by 1 minute
-    $("#decreaseBreak").click(function() {
-        $('#mode').text("Break");
-        var next = $("#breakLength").text();
-        $("#breakLength").text(parseInt(next, 10) - 1);
-        $("#timer").text($('#breakLength').text());
-    });
+    }
+}
+
+
+
+$(document).ready(function () {
+	//sets the starting timer at 25
+	$("#timer").text($('#sessionLength').text());
+	//experimenting with multiple selectors, having trouble evaluating the string "+"
+	/*$("#increase").click(function() {
+	    var modifier = $(this).text();
+	    if (this == "#increaseSession" || this == "#decreaseSession") {
+	        var result = $('#sessionLength').text() + modifier + 1;
+	        $('#sessionLength').text(eval(result));
+	    } else if (this == '#decreaseBreak' || this == "#increaseBreak") {
+	        var result = $('#breakLength').text() + modifier + 1;
+	        $('#breakLength').text(eval(result));
+	    }
+	}); */
+
+	//increases the session length by 1 minutes
+	$("#increaseSession").on('click', function () {
+		$('#mode').text("Session");
+		var next = parseInt($("#sessionLength").text(), 10);
+		$("#sessionLength").text(next + 1);
+		$("#timer").text($('#sessionLength').text());
+	});
+	//decreases the session length by 1 minute
+	$("#decreaseSession").on('click', function () {
+		$('#mode').text("Session");
+		var next = parseInt($("#sessionLength").text(), 10);
+		$("#sessionLength").text(next - 1);
+		$("#timer").text($('#sessionLength').text());
+	});
+	//increases the break length by 1 minute
+	$("#increaseBreak").on('click', function () {
+		$('#mode').text("Break");
+		var next = $("#breakLength").text();
+		$("#breakLength").text(parseInt(next, 10) + 1);
+		$("#timer").text($('#breakLength').text());
+	});
+	//decreases break length by 1 minute
+	$("#decreaseBreak").on('click', function () {
+		$('#mode').text("Break");
+		var next = $("#breakLength").text();
+		$("#breakLength").text(parseInt(next, 10) - 1);
+		$("#timer").text($('#breakLength').text());
+	});
+
+	$('#start').on('click', function () {
+
+	});
+
+
 });

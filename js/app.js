820cb3e81a3ba9bd2f166cf6d6670014d960e230
antcho92 <antcho92@gmail.com>
2016-08-05 22:07:52 -0700

    Changed index.js to app.js

--- js/app.js
+++ js/app.js
@@ -1,39 +1,20 @@
-function doTimer(length, resolution, oninstance, oncomplete) {
-	var steps = (length / 100) * (resolution / 10),
-		speed = length / steps,
-		count = 0,
-		start = new Date().getTime();
-
-	function instance() {
-		if (count++ == steps) {
-			oncomplete(steps, count);
-		} else {
-			oninstance(steps, count);
-
-			var diff = (new Date().getTime() - start) - (count * speed);
-			window.setTimeout(instance, (speed - diff));
-		}
-	}
-
-	window.setTimeout(instance, speed);
+function startTimer() {
+	var timer = $('#timer').text(),
+		reference = new Date().getTime();
+	updateTimer();
 }
 
+function updateTimer() {
+	$('#timer').text();
+}
 
+function format() {
+
+}
 
 $(document).ready(function () {
 	//sets the starting timer at 25
 	$("#timer").text($('#sessionLength').text());
-	//experimenting with multiple selectors, having trouble evaluating the string "+"
-	/*$("#increase").click(function() {
-	    var modifier = $(this).text();
-	    if (this == "#increaseSession" || this == "#decreaseSession") {
-	        var result = $('#sessionLength').text() + modifier + 1;
-	        $('#sessionLength').text(eval(result));
-	    } else if (this == '#decreaseBreak' || this == "#increaseBreak") {
-	        var result = $('#breakLength').text() + modifier + 1;
-	        $('#breakLength').text(eval(result));
-	    }
-	}); */
 
 	//increases the session length by 1 minutes
 	$("#increaseSession").on('click', function () {

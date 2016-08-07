9adc816a69bfab96e5591264f9b687053ec29ef7
antcho92 <antcho92@gmail.com>
2016-08-06 19:51:01 -0700

    Timer working but needs improvement

--- js/app.js
+++ js/app.js
@@ -1,50 +1,57 @@
+//starts the timer and finds the end time using a new Date object and adding the remining time
 function startTimer() {
-	var timeRemaining = $('#timer').text().split(':'),
-		endTime = new Date().getTime() + parseInt(timeRemaining[0], 10) * 60000;
-	updateTimer(timer);
-	console.log(endTime);
+    var timeRemaining = $('#minutes').text() * 60 * 1000 + $('#seconds').text() * 1000,
+        endTime = (new Date()).getTime() + timeRemaining;
+
+    function updateTimer() {
+        tNow = new Date();
+        timeRemaining = endTime - tNow.getTime();
+		//need to change math.floor to round and only get the minutes and seconds by subtracting minutes from the timeremaining
+        secs = Math.floor((timeRemaining / 1000) % 60);
+        mins = Math.floor((timeRemaining / 1000 / 60) % 60);
+        $('#seconds').text(secs);
+        $('#minutes').text(mins);
+        if (timeRemaining <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+    updateTimer();
+    var timeInterval = window.setInterval(updateTimer, 500);
 }
 
-function updateTimer(timer) {
-	$('#timer').text(format(timer));
-}
 
-function format(timer) {
+$(document).ready(function() {
 
-}
+    //increases the session length by 1 minutes
+    $("#increaseSession").on('click', function() {
+        $('#mode').text("Session");
+        var next = parseInt($("#sessionLength").text(), 10);
+        $("#sessionLength").text(next + 1);
+        $("#minutes").text($('#sessionLength').text());
+    });
+    //decreases the session length by 1 minute
+    $("#decreaseSession").on('click', function() {
+        $('#mode').text("Session");
+        var next = parseInt($("#sessionLength").text(), 10);
+        $("#sessionLength").text(next - 1);
+        $("#minutes").text($('#sessionLength').text());
+    });
+    //increases the break length by 1 minute
+    $("#increaseBreak").on('click', function() {
+        $('#mode').text("Break");
+        var next = $("#breakLength").text();
+        $("#breakLength").text(parseInt(next, 10) + 1);
+        $("#minutes").text($('#breakLength').text());
+    });
+    //decreases break length by 1 minute
+    $("#decreaseBreak").on('click', function() {
+        $('#mode').text("Break");
+        var next = $("#breakLength").text();
+        $("#breakLength").text(parseInt(next, 10) - 1);
+        $("#minutes").text($('#breakLength').text());
+    });
 
-$(document).ready(function () {
-
-	//increases the session length by 1 minutes
-	$("#increaseSession").on('click', function () {
-		$('#mode').text("Session");
-		var next = parseInt($("#sessionLength").text(), 10);
-		$("#sessionLength").text(next + 1);
-		$("#timer").text($('#sessionLength').text());
-	});
-	//decreases the session length by 1 minute
-	$("#decreaseSession").on('click', function () {
-		$('#mode').text("Session");
-		var next = parseInt($("#sessionLength").text(), 10);
-		$("#sessionLength").text(next - 1);
-		$("#timer").text($('#sessionLength').text());
-	});
-	//increases the break length by 1 minute
-	$("#increaseBreak").on('click', function () {
-		$('#mode').text("Break");
-		var next = $("#breakLength").text();
-		$("#breakLength").text(parseInt(next, 10) + 1);
-		$("#timer").text($('#breakLength').text());
-	});
-	//decreases break length by 1 minute
-	$("#decreaseBreak").on('click', function () {
-		$('#mode').text("Break");
-		var next = $("#breakLength").text();
-		$("#breakLength").text(parseInt(next, 10) - 1);
-		$("#timer").text($('#breakLength').text());
-	});
-
-	$('#start').on('click', startTimer);
+    $('#start').on('click', startTimer);
 
 
 });
